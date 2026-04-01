befd43a34163e0bd4ff3fa9e8b9dd305ec76c707
bmathews42 <brocmathews@gmail.com>
2026-03-31 21:40:51 -0600

    Update settings.py

--- pilates_logger/settings.py
+++ pilates_logger/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://pilates-tracker.onrender.com']
+ALLOWED_HOSTS = ['pilates-tracker.onrender.com']
 
 
 # Application definition
